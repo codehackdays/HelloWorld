3dea2abf7a948b07ace393d8ab2c6104d6c52443
codehackdays <codehackdays@users.noreply.github.com>
2017-04-17 21:25:51 +0100

    Added Auth0 to Micro Service

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://codehackdays.eu.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://codehackdays-helloworld.herokuapp.com/',
+    issuer: "https://codehackdays.eu.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 var redis = require('redis');
 var redis_client = redis.createClient(process.env.REDIS_URL);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res){
   res.send('hello world');
+});
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
 });
 
 app.get('/sayhello', function (req, res){
