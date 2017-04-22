70378a4d2402bff9e59329581636980aad62acb9
Luke Stankus <liudvikas.stankus@cre.ventures>
2017-04-22 14:27:24 +0100

    3rd attempt

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ if (process.env.AUTH_ENABLED === 'true' ||
 }
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
 });
 
 var _redis_client;
