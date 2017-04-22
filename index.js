68163134f084b40ab1e2ff2aa15b5db4b2c268e5
Luke Stankus <liudvikas.stankus@cre.ventures>
2017-04-22 14:15:19 +0100

    hack to allow all headers

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (process.env.AUTH_ENABLED === 'true' ||
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
